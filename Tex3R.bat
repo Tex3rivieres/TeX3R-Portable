d1bc2d5060fe595519a99755475227c427406e6e
Tex3rivieres <132594586+Tex3rivieres@users.noreply.github.com>
2023-11-19 19:20:27 +0100

    Add files via upload

---
+++ Tex3R.bat
@@ -0,0 +1,6 @@
+cd /d %~dp0
+set PATH=%cd%\miktex\texmfs\install\miktex\bin\x64;%cd%\fonts;%cd%\Tex3R;%cd%\python;%cd%\Sumatra;%PATH%
+set OSFONTDIR=%cd%\fonts
+start "" "%cd%\VSCodium\VSCodium.exe"
+
+
---
+++ update-teX3R.bat
@@ -0,0 +1,17 @@
+﻿cd /d %~dp0
+set PATH=%cd%\PortableGit\cmd;%cd%\Tex3R;%PATH%
+
+SET REPO_PATH=%cd%\TeX3R
+@echo off
+chcp 65001
+
+@echo on
+echo Mise à jour du dépôt Git...
+
+
+
+
+cd %REPO_PATH%
+git pull
+echo Mise à jour terminée.
+pause
